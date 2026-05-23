54fd3c9598f9f0d71d535712ee965f41dfd04e89
idvenmanurung <jrmanurung2@gmail.com>
2026-05-24 01:51:47 +0700

    new update

--- pr07/index.js
+++ pr07/index.js
@@ -4,7 +4,7 @@ const app = express()
 app.get('/', (req, res) => res.json({ message: 'Hello World' })) 
 app.get('/health', (req, res) => res.json({ status: 'ok' })) 
  
-const server = app.listen(3000, () => console.log('Running on port 30000')) 
+const server = app.listen(3000, () => console.log('Running on port 3000')) 
 
  
 process.on('SIGTERM', () => { 
