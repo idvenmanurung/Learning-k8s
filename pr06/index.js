d30da77241bb2040cf7cb6553dd138bcf40f287f
idvenmanurung <jrmanurung2@gmail.com>
2026-05-24 01:59:51 +0700

    fixing update

---
+++ pr06/index.js
@@ -0,0 +1,16 @@
+const express = require('express') 
+const app = express() 
+ 
+app.get('/', (req, res) => res.json({ message: 'Hello World' })) 
+app.get('/health', (req, res) => res.json({ status: 'ok' })) 
+ 
+const server = app.listen(3000, () => console.log('Running on port 30000')) 
+
+ 
+process.on('SIGTERM', () => { 
+  console.log('SIGTERM received, shutting down...') 
+  server.close(() => { 
+    console.log('Server closed') 
+    process.exit(0) 
+  }) 
+}) 
